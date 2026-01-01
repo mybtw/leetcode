26365a681df392d3ee6a126d9f3ac0a92210690a
Ilya Astafurov <ilyabigk@gmail.com>
2026-01-01 14:36:29 +0300

    Add maximum score and plus one

--- src/main/kotlin/PlusOne.kt
+++ src/main/kotlin/PlusOne.kt
@@ -1,20 +1,14 @@
 package su.china
 
-// you can iterate form last elem and keep track of suffixMin and prefixSum instead of using 2 arrays
-fun maximumScore(nums: IntArray): Long {
-    val prefixSum = LongArray(nums.size) { 0 }
-    prefixSum[0] = nums[0].toLong()
-    for (i in 1..nums.size - 1) {
-        prefixSum[i] = prefixSum[i - 1] + nums[i]
+fun plusOne(digits: IntArray): IntArray {
+    for (i in digits.size - 1 downTo 0) {
+        if (digits[i] < 9) {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0
     }
-    val suffixMin = IntArray(nums.size) { 0 }
-    suffixMin[nums.size - 1] = nums[nums.size - 1]
-    for (i in nums.size - 2 downTo 0) {
-        suffixMin[i] = nums[i].coerceAtMost(suffixMin[i + 1])
-    }
-    var res: Long = Long.MIN_VALUE
-    for (i in 0..nums.size - 2) {
-        res = res.coerceAtLeast(prefixSum[i] - suffixMin[i + 1])
-    }
+    val res = IntArray(digits.size + 1) { 0 }
+    res[0] = 1
     return res
 }
